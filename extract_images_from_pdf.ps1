5a4c61abc781dbe3340b35005858dca797780cbd
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2022-08-18 13:59:53 +0200

    added script extracting images from PDF

---
+++ extract_images_from_pdf.ps1
@@ -0,0 +1,13 @@
+pdfimages -j file.pdf prefix
+
+# Output images will be:
+#  - prefix-0001.jpg
+#  - prefix-0002.jpg
+#  - prefix-0003.jpg
+#  ...
+
+
+
+
+# References:
+#  - https://www.xpdfreader.com/pdfimages-man.html
