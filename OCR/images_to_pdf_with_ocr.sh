acede82dc59e561629934606a25a6488ac1f8e8e
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2023-01-05 15:04:18 +0100

    added script to merge images to single OCR'ed PDF

---
+++ OCR/images_to_pdf_with_ocr.sh
@@ -0,0 +1,25 @@
+
+# PROCEDURE 1: ImageMagick + OCRmyPDF
+
+# Merge images to a single PDF file.
+# @see https://askubuntu.com/questions/493584/convert-images-to-pdf
+cd path/to/image/folder
+convert "*.{jpg}" -quality 100 outfile.pdf
+
+# Above command could be prevented by below error:
+# convert-im6.q16: attempt to perform an operation not allowed by the security policy `PDF' @ error/constitute.c/IsCoderAuthorized/421.
+# If so, file /etc/ImageMagick-6/policy.xml needs to be patched,
+# as suggested by https://stackoverflow.com/questions/52998331/imagemagick-security-policy-pdf-blocking-conversion#59193253
+
+# OCR the PDF just created.
+# @see https://ocrmypdf.readthedocs.io/en/latest/cookbook.html#basic-examples
+ocrmypdf -l ita outfile.pdf outfile_ocr.pdf
+
+# ######################################################################################################################
+
+
+# PROCEDURE 2: Tesseract
+
+# @see https://ocrmypdf.readthedocs.io/en/latest/cookbook.html#ocr-images-not-pdfs
+cd path/to/image/folder
+tesseract <( ls -1 ) output-prefix pdf -l ita
