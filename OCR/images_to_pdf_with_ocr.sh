198fd9512b1b1395464ffc7ded4fcb933c433a7e
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2024-03-08 09:56:04 +0100

    updated script to run OCR on images and create PDF

--- OCR/images_to_pdf_with_ocr.sh
+++ OCR/images_to_pdf_with_ocr.sh
@@ -7,7 +7,7 @@ cd path/to/image/folder
 convert "*.{jpg}" -quality 100 outfile.pdf
 
 # Above command could be prevented by below error:
-# convert-im6.q16: attempt to perform an operation not allowed by the security policy `PDF' @ error/constitute.c/IsCoderAuthorized/421.
+#   convert-im6.q16: attempt to perform an operation not allowed by the security policy `PDF' @ error/constitute.c/IsCoderAuthorized/421.
 # If so, file /etc/ImageMagick-6/policy.xml needs to be patched,
 # as suggested by https://stackoverflow.com/questions/52998331/imagemagick-security-policy-pdf-blocking-conversion#59193253
 
@@ -25,6 +25,5 @@ ocrmypdf -l ita outfile.pdf outfile_ocr.pdf
 
 # PROCEDURE 2: Tesseract
 
-# @see https://ocrmypdf.readthedocs.io/en/latest/cookbook.html#ocr-images-not-pdfs
 cd path/to/image/folder
-tesseract <( ls -1 ) output-prefix pdf -l ita
+tesseract <( ls -1 *.jpg ) testo -l ita pdf
